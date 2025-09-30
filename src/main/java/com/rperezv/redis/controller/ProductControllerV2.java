6222e8ad9e054893d90a4a9d48d104205654a752
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-09-30 17:37:17 +0200

    105. Product Service - V2

---
+++ src/main/java/com/rperezv/redis/controller/ProductControllerV2.java
@@ -0,0 +1,48 @@
+package com.rperezv.redis.controller;
+
+import com.rperezv.redis.entity.Product;
+import com.rperezv.redis.service.ProductServiceV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+/**
+ * ProductController
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 13/09/2025 - 15:52
+ * @since 1.17
+ */
+@RestController
+@RequestMapping("product/v2")
+@RequiredArgsConstructor
+@Slf4j
+public class ProductControllerV2 {
+
+    private final ProductServiceV2 service;
+
+    @GetMapping("{id}")
+    public Mono<Product> getProductById(@PathVariable Integer id) {
+        log.info("Getting product (in controller) by id: {}", id);
+
+        return this.service.getProductById(id);
+    }
+
+    @PutMapping("{id}")
+    public Mono<Product> updateProductById(@PathVariable Integer id, @RequestBody Mono<Product> product) {
+        log.info("Updating product (in controller) by id: {}", id);
+
+        return this.service.updateProduct(id, product);
+    }
+
+    @DeleteMapping("{id}")
+    public Mono<Void> deleteProductById(@PathVariable Integer id) {
+        log.info("Deleting product (in controller) by id: {}", id);
+
+        return this.service.deleteProduct(id);
+    }
+
+}
--- src/main/java/com/rperezv/redis/service/ProductServiceV2.java
+++ src/main/java/com/rperezv/redis/service/ProductServiceV2.java
@@ -1,5 +1,12 @@
 package com.rperezv.redis.service;
 
+import com.rperezv.redis.entity.Product;
+import com.rperezv.redis.service.util.CacheTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
 /**
  * ProductServiceV2
  * <p>
@@ -9,5 +16,33 @@ package com.rperezv.redis.service;
  * @version 21/09/2025 - 14:42
  * @since 1.17
  */
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductServiceV2 {
+
+    private final CacheTemplate<Integer, Product> cacheTemplate;
+
+    // GET
+    public Mono<Product> getProductById(Integer id) {
+        log.info("Get product (in service) by id: {}", id);
+
+        return this.cacheTemplate.get(id);
+    }
+
+    // PUT
+    public Mono<Product> updateProduct(Integer id, Mono<Product> product) {
+        log.info("Update product (in service) by id: {}", product);
+
+        return product
+                .flatMap(p -> this.cacheTemplate.update(id, p));
+    }
+
+    // DELETE
+    public Mono<Void> deleteProduct(Integer id) {
+        log.info("Delete product (in service) by id: {}", id);
+
+        return this.cacheTemplate.delete(id);
+    }
+
 }
